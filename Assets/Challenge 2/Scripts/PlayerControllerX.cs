21553d7ff509a22acbb76bb14b5a71f37efb66b5
ManelNeon <neonextra0@gmail.com>
2024-02-28 15:42:50 +0000

    Challenge 2
    Conteúdos do Challenge 2, com os dois bonus challenges, o de randomizar o spawn interval e o de impedir o jogador de spammar o spacebar.

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    //timer que vai ver quanto tempo passou depois de spawnar um cão
+    float dogCooldownTimer;
+
+    //timer que diz quanto cooldown é que há entre spawns de cães
+    [SerializeField] float dogCooldown = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        dogCooldownTimer += Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && dogCooldownTimer > dogCooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            dogCooldownTimer = 0f;
+        }
+    }
+}
